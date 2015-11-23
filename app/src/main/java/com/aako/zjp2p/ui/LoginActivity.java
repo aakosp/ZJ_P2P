0292439e133f7347577a8d148a9e5cfb3cfd611c
aako <aakosp@gmail.com>
2015-11-23 23:26:18 +0800

    creat

---
+++ app/src/main/java/com/aako/zjp2p/ui/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.aako.zjp2p.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ZL on 2015/11/23.
+ */
+public class LoginActivity extends Activity {
+    private static final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
