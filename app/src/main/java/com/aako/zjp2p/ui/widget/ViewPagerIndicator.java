e386ed0f06e70ca756ef5000c176a1a6871b8178
aako <aakosp@gmail.com>
2015-12-17 23:21:37 +0800

    add widget

---
+++ app/src/main/java/com/aako/zjp2p/ui/widget/ViewPagerIndicator.java
@@ -0,0 +1,25 @@
+package com.aako.zjp2p.ui.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by aako on 2015/12/17.
+ */
+public class ViewPagerIndicator extends LinearLayout {
+
+    public ViewPagerIndicator(Context context) {
+        this(context, null);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ViewPagerIndicator(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    
+}
